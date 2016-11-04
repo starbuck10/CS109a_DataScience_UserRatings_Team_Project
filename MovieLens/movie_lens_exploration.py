d4585446259c44b6e9140b489aed30a6035ba938
Timur Zambalayev <timur@jetbrains.com>
2016-11-04 18:58:43 -0400

    show_ratings_histogram

--- MovieLens/movie_lens_exploration.py
+++ MovieLens/movie_lens_exploration.py
@@ -1,6 +1,7 @@
 from collections import namedtuple, Counter
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 Dataset = namedtuple('Dataset', ['ratings_df', 'movies_df'])
@@ -10,6 +11,24 @@ def read_data():
     ratings_df = pd.read_csv('ml-latest-small/ratings.csv')
     movies_df = pd.read_csv('ml-latest-small/movies.csv')
     return Dataset(ratings_df, movies_df)
+
+
+def show_ratings_histogram(ratings):
+    _, ax = plt.subplots(1, 1, figsize=(15, 10))
+
+    ax.hist(ratings, bins=np.arange(0.25, 5.5, step=0.5), alpha=0.4)
+
+    ratings_mean = ratings.mean()
+
+    ax.axvline(x=ratings_mean, linewidth=3, color='k')
+    plt.text(ratings_mean - 0.55, 25000, 'mean = %.2f' % ratings_mean)
+
+    ax.set_xlabel('rating')
+    ax.set_ylabel('count')
+    ax.set_title('Overall ratings histogram')
+
+    plt.tight_layout()
+    plt.show()
 
 
 def explore(dataset):
@@ -32,9 +51,7 @@ def explore(dataset):
     print 'Unique rating values:', sorted(ratings_counter.keys())
     print 'Overall ratings mean: {:.2f}'.format(ratings.mean())
 
-    ratings.hist()
-
-    plt.show()
+    show_ratings_histogram(ratings)
 
 
 def main():
