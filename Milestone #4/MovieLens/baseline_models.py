d90be8b19bff6db8fe27662735b346d4664f08ed
Timur Zambalayev <timur@jetbrains.com>
2016-11-27 14:33:41 -0500

    mean movie ratings model

--- Milestone #4/MovieLens/baseline_models.py
+++ Milestone #4/MovieLens/baseline_models.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from datetime import datetime
 
 import matplotlib.pyplot as plt
@@ -157,14 +158,93 @@ def fit_mean_user_ratings_model(ratings_df):
     print 'mean test score: %.4f, std: %.4f' % (np.mean(test_scores), np.std(test_scores))
 
 
+def get_mean_movie_ratings(ratings_df):
+    return ratings_df.groupby('movieId')['rating'].mean()
+
+
+def show_movie_mean_ratings_histogram(ratings_df):
+    movie_mean_ratings = get_mean_movie_ratings(ratings_df)
+
+    print 'The maximum movie mean rating: %.2f' % movie_mean_ratings.max()
+    movie_ratings_mean = movie_mean_ratings.mean()
+    print 'The mean movie mean rating: %.2f' % movie_ratings_mean
+    print 'The minimum movie mean rating: %.2f' % movie_mean_ratings.min()
+
+    _, ax = plt.subplots(1, 1, figsize=get_fig_size())
+
+    ax.hist(movie_mean_ratings, bins=50, alpha=0.4)
+
+    ax.axvline(x=movie_ratings_mean, linewidth=3, color='k')
+    plt.text(movie_ratings_mean + 0.05, 1150, 'mean = %.2f' % movie_ratings_mean)
+
+    ax.set_xlabel('movie mean rating')
+    ax.set_ylabel('count')
+    ax.set_title('Movie mean ratings')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def explore_mean_movie_ratings(ratings_df):
+    movie_ids = ratings_df['movieId']
+
+    print 'How many movies? {:,}\n'.format(len(set(movie_ids)))
+
+    num_ratings_per_movie = Counter(movie_ids).values()
+
+    mean = np.mean(num_ratings_per_movie)
+    print 'The maximum number of ratings per movie: %.0f' % np.max(num_ratings_per_movie)
+    print 'The mean number of ratings per movie: %.2f' % mean
+    print 'The minimum number of ratings per movie: %.0f\n' % np.min(num_ratings_per_movie)
+
+    num_ratings_per_movie_counter = Counter(num_ratings_per_movie)
+
+    print 'Number of movies with one rating: {:,}'.format(num_ratings_per_movie_counter[1])
+    print 'Number of movies with two ratings: {:,}\n'.format(num_ratings_per_movie_counter[2])
+
+    show_movie_mean_ratings_histogram(ratings_df)
+
+
+def get_y_pred_mean_movie_ratings_model(mean_movie_ratings, mean_user_ratings, x):
+    return [mean_movie_ratings.get(row['movieId'], mean_user_ratings[row['userId']]) for _, row in x.iterrows()]
+
+
+def fit_mean_movie_ratings_model(ratings_df):
+    train_scores = []
+    test_scores = []
+    n_iter = 4
+    for _ in xrange(n_iter):
+        train_df, test_df = train_test_split(ratings_df)
+
+        x_train, y_train = get_xy(train_df)
+        x_test, y_test = get_xy(test_df)
+
+        mean_movie_ratings = get_mean_movie_ratings(train_df)
+        mean_user_ratings = get_mean_user_ratings(train_df)
+
+        y_train_pred = get_y_pred_mean_movie_ratings_model(mean_movie_ratings, mean_user_ratings, x_train)
+        y_test_pred = get_y_pred_mean_movie_ratings_model(mean_movie_ratings, mean_user_ratings, x_test)
+
+        train_score, test_score = get_scores(y_test, y_test_pred, y_train, y_train_pred)
+
+        train_scores.append(train_score)
+        test_scores.append(test_score)
+
+    print 'mean train score: %.4f, std: %.4f' % (np.mean(train_scores), np.std(train_scores))
+    print 'mean test score: %.4f, std: %.4f' % (np.mean(test_scores), np.std(test_scores))
+
+
 def main():
     ratings_df = read_data()
 
     # explore_total_mean_rating(ratings_df)
     # fit_total_mean_model(ratings_df)
 
-    explore_mean_user_ratings(ratings_df)
-    fit_mean_user_ratings_model(ratings_df)
+    # explore_mean_user_ratings(ratings_df)
+    # fit_mean_user_ratings_model(ratings_df)
+
+    explore_mean_movie_ratings(ratings_df)
+    fit_mean_movie_ratings_model(ratings_df)
 
 
 if __name__ == '__main__':
