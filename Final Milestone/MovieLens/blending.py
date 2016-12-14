5ca55f734d0d54730f250a6084cb09577c38d5a8
Timur Zambalayev <timur@jetbrains.com>
2016-12-14 14:49:52 -0500

    blending model results

--- Final Milestone/MovieLens/blending.py
+++ Final Milestone/MovieLens/blending.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
@@ -5,21 +7,17 @@ from sklearn.model_selection import train_test_split
 
 from baseline_models import BaselineEffectsModel
 from baseline_models import BaselineMeansModel
-from baseline_models import root_mean_squared_error
 from common import elapsed_time
 from common import get_xy
+from common import root_mean_squared_error
 from movie_similarity_model import MovieSimilarityModel
 from read_ratings import read_ratings_df_with_timestamp
+from user_similarity_model import UserSimilarityModel
 
 
 class BlendingModel(object):
-    def __init__(self):
-        self.models = [
-            BaselineMeansModel(user_weight=0.0),
-            BaselineMeansModel(user_weight=1.0),
-            BaselineEffectsModel(movie_lambda=5.0, user_lambda=20.0),
-            MovieSimilarityModel(),
-        ]
+    def __init__(self, models):
+        self.models = models
         self.regression = LinearRegression()
 
     def get_blend_predictions(self, x):
@@ -55,7 +53,8 @@ class BlendingModel(object):
 
             self.regression.fit(blend_predictions, y)
 
-            print self.regression.coef_, self.regression.intercept_
+            print 'linear regression coefficients: %s, intercept: %.3f' % (
+                self.regression.coef_, self.regression.intercept_)
 
     def predict(self, x):
         blend_predictions = self.get_blend_predictions(x)
@@ -63,33 +62,67 @@ class BlendingModel(object):
         return self.regression.predict(blend_predictions)
 
 
-def build_blending_model(ratings_df):
-    model = BlendingModel()
+ModelRecord = namedtuple('ModelRecord', ['name', 'model'])
 
+
+def score_models(ratings_df, model_records):
     train_ratings_df, test_ratings_df = train_test_split(ratings_df)
     x_train, y_train = get_xy(train_ratings_df)
     x_test, y_test = get_xy(test_ratings_df)
 
-    model.fit(train_ratings_df)
+    print
 
-    y_train_pred = model.predict(x_train)
-    y_test_pred = model.predict(x_test)
+    for model_record in model_records:
+        model_name, model = model_record
+        model.fit(train_ratings_df)
 
-    train_r2_score = r2_score(y_train, y_train_pred)
-    test_r2_score = r2_score(y_test, y_test_pred)
+        y_train_pred = model.predict(x_train)
+        y_test_pred = model.predict(x_test)
 
-    train_rmse = root_mean_squared_error(y_train, y_train_pred)
-    test_rmse = root_mean_squared_error(y_test, y_test_pred)
+        train_r2_score = r2_score(y_train, y_train_pred)
+        test_r2_score = r2_score(y_test, y_test_pred)
 
-    print 'train r2 score: %.4f, test r2 score: %.4f' % (train_r2_score, test_r2_score)
-    print 'train rmse: %.4f, test rmse: %.4f' % (train_rmse, test_rmse)
+        train_rmse = root_mean_squared_error(y_train, y_train_pred)
+        test_rmse = root_mean_squared_error(y_test, y_test_pred)
+
+        print '%s' % model_name
+        print 'train r2 score: %.4f, test r2 score: %.4f' % (train_r2_score, test_r2_score)
+        print 'train rmse: %.4f, test rmse: %.4f\n' % (train_rmse, test_rmse)
+
+
+def score_baseline_mean_blending_model(ratings_df):
+    blend_models = [
+        BaselineMeansModel(user_weight=0.0),
+        BaselineMeansModel(user_weight=1.0),
+    ]
+    models = [
+        ModelRecord('movie mean model', BaselineMeansModel(user_weight=0.0)),
+        ModelRecord('user mean model', BaselineMeansModel(user_weight=1.0)),
+        ModelRecord('blending model (user & movie mean models)', BlendingModel(blend_models)),
+    ]
+    score_models(ratings_df, models)
+
+
+def score_include_all_blending_model(ratings_df):
+    blend_models = [
+        BaselineMeansModel(user_weight=0.0),
+        BaselineMeansModel(user_weight=1.0),
+        BaselineEffectsModel(movie_lambda=5.0, user_lambda=20.0),
+        MovieSimilarityModel(k_neighbors=40),
+        UserSimilarityModel(k_neighbors=30),
+    ]
+    models = [
+        ModelRecord('blending model (user & movie mean models)', BlendingModel(blend_models)),
+    ]
+    score_models(ratings_df, models)
 
 
 def main():
     ratings_df = read_ratings_df_with_timestamp('ml-latest-small/ratings.csv')
     # ratings_df = read_ratings_df('ml-latest-small/ratings_5_pct.csv')
 
-    build_blending_model(ratings_df)
+    # score_baseline_mean_blending_model(ratings_df)
+    score_include_all_blending_model(ratings_df)
 
 
 if __name__ == '__main__':
